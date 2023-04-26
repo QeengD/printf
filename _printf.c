4b68facdc6918a08a26bb123f3058088fb856403
QeengD <QeengD>
2023-04-26 03:42:21 -0700

    Printf Project

--- _printf.c
+++ _printf.c
@@ -1,66 +1,52 @@
 #include "main.h"
 
-void print_temp(char temp[], int *buff_ind);
-
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int switch, width, precision, size, buff_ind = 0;
-	va_list list;
-	char temp[BUFF_SIZE];
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *temp;
 
-	if (format == NULL)
+	va_start(arguments, format), temp = malloc(sizeof(char) * 1024);
+	if (!format || !temp || (format[i] == '%' && !format[i + 1]))
 		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
 	{
-		if (format[i] != '%')
+		if (format[i] == '%')
 		{
-			temp[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_temp(temp, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			if (format[i + 1] == '\0')
+			{	print_buf(temp, ibuf), free(temp), va_end(arguments);
+				return (-1);
+			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(temp, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, temp, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
 		}
 		else
-		{
-			print_temp(temp, &buff_ind);
-			switch = get_switch(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, temp,
-				switch, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
+			handl_buf(temp, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
 	}
-
-	print_temp(temp, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+	print_buf(temp, ibuf), free(temp), va_end(arguments);
+	return (len);
 }
 
-/**
- * print_temp - Prints the contents of the temp if it exist
- * @temp: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_temp(char temp[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &temp[0], *buff_ind);
-
-	*buff_ind = 0;
-}
